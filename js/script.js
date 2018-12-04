17a23b19f0a4a224ade37e9f44263263465f15ae
Dariusz Rosiek <daro.krk@interia.pl>
2018-12-04 22:18:09 +0100

    Finish RWD breakpoints page model

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
-const navBtn = document.querySelector(".mobile__wrapper");
-const navMenu = document.querySelector(".home__nav");
-navBtn.addEventListener('click', function () {
-    navBtn.classList.toggle('change');
-    navMenu.classList.toggle('change__menu');
+document.addEventListener("DOMContentLoaded", function () {
+
+    const navBtn = document.querySelector(".mobile__wrapper");
+    const navMenu = document.querySelector(".home__nav");
+    navBtn.addEventListener('click', function () {
+        navBtn.classList.toggle('change');
+        navMenu.classList.toggle('change__menu');
+    });
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
 });
